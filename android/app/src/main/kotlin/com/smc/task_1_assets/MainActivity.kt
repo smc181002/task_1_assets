23e8fee4a9e9f084e21529c3356d0a7d1cbf823d
smc181002 <meherchaitanya18802@gmail.com>
2020-08-06 00:04:34 +0530

    audio app v1.0.0

---
+++ android/app/src/main/kotlin/com/smc/task_1_assets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.smc.task_1_assets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
+//import 'package:audioplayers/audio_cache.dart';
+//import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(MyApp());
+}
+//AudioCache cache = AudioCache();
+//AudioPlayer player = AudioPlayer();
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Hello World',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'RedHatDisplay',
+      ),
+      home: MyHomePage(),
+    );
+
+  }
+}
+final assetsAudioPlayer = AssetsAudioPlayer();
+// ignore: must_be_immutable
+class MyHomePage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          offline,
+          online,
+          SizedBox(height: 80),
+        ],
+      ),
+      backgroundColor: Color(0xFFFEFEFE),
+    );
+  }
+
+  Widget offline = Container(
+//    height: 720,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(height: 80),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 60.0),
+          alignment: Alignment.center,
+          width: double.maxFinite,
+          height: 270.0,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  color: Color(0x36000000),
+                  blurRadius: 32.0,
+                  spreadRadius: 0,
+                  offset: Offset(0, 16.0)
+              ),
+              BoxShadow(
+                  color: Color(0x12000000),
+                  blurRadius: 32.0,
+                  spreadRadius: 0,
+                  offset: Offset(0, 8.0)
+              )
+            ],
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage('https://images.pexels.com/photos/1616403/pexels-photo-1616403.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')),
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            color: Colors.blueAccent,
+          ),
+        ),
+        SizedBox(height: 40),
+        Text(
+          'Duel Of The Fates',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 8),
+        Text(
+          'By John Williams',
+          style: TextStyle(
+              color: Color(0x99000000)
+          ),
+        ),
+        SizedBox(height: 72),
+        Text('This is audio form Assets'),
+        SizedBox(height: 8),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+                icon: Icon(Icons.play_arrow),
+                iconSize: 36,
+                onPressed: () async {
+                  await assetsAudioPlayer.open(
+                    Audio('assets/dueloffates.mp3'),
+                  );
+                }
+            ),
+            IconButton(
+              icon: Icon(Icons.pause),
+              iconSize: 36,
+              onPressed: () => assetsAudioPlayer.playOrPause(),
+//              onPressed: () => print(height.round()),
+            ),
+            IconButton(
+              icon: Icon(Icons.stop),
+              iconSize: 36,
+              onPressed: () => assetsAudioPlayer.stop(),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+
+  Widget online = Container(
+//    height: 720,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(height: 80),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 60.0),
+          alignment: Alignment.center,
+          width: double.maxFinite,
+          height: 270.0,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  color: Color(0x36000000),
+                  blurRadius: 32.0,
+                  spreadRadius: 0,
+                  offset: Offset(0, 16.0)
+              ),
+              BoxShadow(
+                  color: Color(0x12000000),
+                  blurRadius: 32.0,
+                  spreadRadius: 0,
+                  offset: Offset(0, 8.0)
+              )
+            ],
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage('https://images.pexels.com/photos/2110951/pexels-photo-2110951.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')),
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            color: Colors.blueAccent,
+          ),
+        ),
+        SizedBox(height: 40),
+        Text(
+          'The Imperial March',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 8),
+        Text(
+          'By John Williams',
+          style: TextStyle(
+              color: Color(0x99000000)
+          ),
+        ),
+        SizedBox(height: 72),
+        Text('This is audio form Network'),
+        SizedBox(height: 8),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+                icon: Icon(Icons.play_arrow),
+                iconSize: 36,
+                onPressed: () async {
+                  await assetsAudioPlayer.open(
+                      Audio.network("https://raw.githubusercontent.com/smc181002/media_files/master/imperialmarch.mp3")
+                  );
+                }
+            ),
+            IconButton(
+              icon: Icon(Icons.pause),
+              iconSize: 36,
+              onPressed: () => assetsAudioPlayer.playOrPause(),
+//              onPressed: () => print(height.round()),
+            ),
+            IconButton(
+              icon: Icon(Icons.stop),
+              iconSize: 36,
+              onPressed: () => assetsAudioPlayer.stop(),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
